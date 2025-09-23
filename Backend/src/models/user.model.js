7c273980c6ac4cc79d0f2d375cdf1618d672c9e6
Ronit865 <Rajdhimmar4@gmail.com>
2025-09-23 11:50:40 +0530

    fix: remove uniqueness constraint from username field in user schema

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -6,7 +6,6 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Username is required'],
-    unique: [true, 'Username must be unique'],
     trim: true,
     index: true,
     minlength: [3, 'Username must be at least 3 characters long']
