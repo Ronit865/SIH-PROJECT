f98eb2a6d184c5ca0412e18f52f781a88c6012ee
Ronit865 <Rajdhimmar4@gmail.com>
2025-09-25 14:24:33 +0530

    fix: update CORS origin to allow specific frontend domain

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 //Basic middleware
 app.use(cors({
-  origin: 'https://sih-project-pojd.onrender.com'||'http://localhost:5173', 
+  origin: ['https://alumnihub-frontend.vercel.app','http://localhost:5173'], 
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Set-Cookie', 'Cookie', 'X-Requested-With'],
