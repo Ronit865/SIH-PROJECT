1bc4d73ec91858d8575b6d4eb73454cb9239442f
Ronit865 <Rajdhimmar4@gmail.com>
2025-09-25 14:14:02 +0530

    fix: update allowedHeaders in CORS middleware to include additional headers

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(cors({
   origin: ['https://sih-project-pojd.onrender.com','http://localhost:5173'], 
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'Set-Cookie', 'Cookie', 'X-Requested-With'],
 }));
 
 app.use(express.json({limit:"16kb"}));  //limit incoming data
