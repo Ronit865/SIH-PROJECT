e098ea3754bf0567df872fdf66db292a1b6ba59c
Ronit865 <Rajdhimmar4@gmail.com>
2025-09-25 12:59:49 +0530

    fix: update CORS origin to allow multiple domains

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 //Basic middleware
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: ['https://sih-project-pojd.onrender.com','http://localhost:5173'], 
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
