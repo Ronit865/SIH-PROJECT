bdbaa63a670d4b81540f12c6b722363e8150fe2b
Ronit865 <Rajdhimmar4@gmail.com>
2025-12-17 20:40:28 +0530

    refactor: simplify port handling in server setup

--- src/index.js
+++ src/index.js
@@ -11,14 +11,18 @@ dotenv.config({
   debug: false
 });
 
+const PORT = process.env.PORT || 5000;
+
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT,()=>{
-        console.log(`Server is running on port ${process.env.PORT || 5000}`);
-        // console.log("Database connection established successfully.");
-    })
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
   })
   .catch((error) => {
     console.error("Failed to connect to the database:", error);
-    process.exit(1); // Exit the process with failure
+    process.exit(1);
   });
+
+
+  
