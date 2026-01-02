856d83a89d2086f46e062381b6c8bcace31836a7
Ronit865 <Rajdhimmar4@gmail.com>
2026-01-02 18:53:57 +0530

    fix: remove unnecessary JWT verification from all user route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,6 @@ import express from 'express';
 import { changeUserPassword,  getAllUser,  getCurrentUser,  getUserById,  updateUserAvatar,  updateUserDetails} from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
 import { verifyJWT, verifyUserOrAdmin } from '../middlewares/auth.middleware.js';
-import User from '../models/user.model.js';
 
 const router = express.Router();
 
@@ -10,13 +9,13 @@ const router = express.Router();
 
 // router.route("/logout").post(verifyJWT, logoutUser)
 
-router.route("/alluser").get(verifyJWT, getAllUser)
+router.route("/alluser").get(getAllUser)
 
 router.route("/change-password").post(verifyJWT, changeUserPassword)
 
 router.route("/user").get(verifyUserOrAdmin, getCurrentUser)
 
-router.route("/:userId").get(verifyJWT, getUserById)
+router.route("/:userId").get( getUserById)
 
 router.route("/update-user").patch(verifyJWT, updateUserDetails)
 
